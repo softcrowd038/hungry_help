dad3bfdde91abb3e1b32c769e2e13f80d83b6ea8
softcrowd038 <softcrowdtest038@gmail.com>
2024-10-09 16:40:05 +0530

    Change in test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:akshay_patra/main.dart';
+import 'package:quick_social/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
