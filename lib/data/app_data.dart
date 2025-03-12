faa31fdcf92ff025ff965c105ef4304e08883562
unknown <softcrowdtest038@gmail.com>
2025-03-12 17:50:27 +0530

    all done

--- lib/data/app_data.dart
+++ lib/data/app_data.dart
@@ -1,13 +1,3 @@
-class AppData {
-  List<String> addresses = [
-    '1234 Elm Street Springfield, IL 62704 United States',
-    '5678 Oak Avenue San Francisco, CA 94110 United States',
-    '4321 Pine Lane Austin, TX 73301 United States',
-    '8765 Maple Boulevard Seattle, WA 98101 United States',
-    '2468 Birch Road Miami, FL 33101 United States',
-  ];
-}
+String baseUrl = 'https://patra-akshay.co.codiantsolutions.com/api/v1';
 
-String baseUrl = 'http://192.168.1.18:8080/api/v1';
-
-String imageBaseUrl = 'http://192.168.1.18:8080/';
+String imageBaseUrl = 'https://patra-akshay.co.codiantsolutions.com/';
